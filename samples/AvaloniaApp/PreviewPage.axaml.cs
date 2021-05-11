61b04a7b44a67680673f7fec155f9eaf8ab37dad
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-11 09:29:01 +0200

    Added PreviewPage

---
+++ samples/AvaloniaApp/PreviewPage.axaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApp
+{
+    public class PreviewPage : UserControl
+    {
+        public PreviewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
+
