4a5b58f5a7be7764d1779f25cd7c64264e9c6499
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-15 14:30:44 +0100

    Add ColorsPage

---
+++ samples/AvaloniaApp/ColorsPage.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApp;
+
+public partial class ColorsPage : UserControl
+{
+    public ColorsPage()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
